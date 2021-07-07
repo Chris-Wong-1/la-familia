270b69c85b4f3f3ccde2005bf3a8bedae3d16b42
chris-wong-1 <christopher.wong@sap.com>
2021-07-07 16:14:18 -0700

    Update main body

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,28 @@
 import React from "react"
 
+const footerStyles = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  fontFamily: "Oswald, serif",
+  fontSize: "1.2rem",
+  letterSpacing: "0.1rem",
+}
+
+const phoneNumber = {
+  textDecoration: "none",
+  color: "inherit",
+}
+
 export default function Footer() {
   return (
-    <div>
+    <div style={footerStyles}>
+      <div>
+        <a style={phoneNumber} href="tel:1-510-548-3420">(510) 548-3420</a>
+      </div>
+      <div>Open Monday - Saturday, 10AM - 9PM</div>
+      <div>Closed Sundays</div>
       <div>2971 Shattuck Avenue, Berkeley, California</div>
-      <div>(510) 548-3420</div>
     </div>
   )
 }
